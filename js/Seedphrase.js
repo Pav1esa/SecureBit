1bd5429c65f309cc7bf4b31e3042ea5b12f4505e
Oleh Kuchma <oleg.kuchma11@gmail.com>
2023-11-02 23:45:41 +0200

    Redesign of seedphrase
    Changed the design of seedphrase page and added js to generate 12 words

---
+++ js/Seedphrase.js
@@ -0,0 +1,32 @@
+const wordlist = [
+    "apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew", "kiwi", "lemon", "mango", "nectarine",
+    "orange", "papaya", "quince", "raspberry", "strawberry", "tangerine", "watermelon", "blueberry", "blackberry", "cranberry",
+    "apricot", "peach", "pear", "plum", "pineapple", "lime", "coconut", "guava", "kiwifruit", "pomegranate", "melon", "grapefruit"
+];
+
+function generateRandomWord() {
+    const randomIndex = Math.floor(Math.random() * wordlist.length);
+    return wordlist[randomIndex];
+}
+
+function generateSeedPhrase() {
+    const seedPhrase = [];
+    for (let i = 0; i < 12; i++) { // Generate 12 random words
+        const randomWord = generateRandomWord();
+        seedPhrase.push(randomWord);
+    }
+    return seedPhrase;
+}
+
+const generateBtn = document.getElementById("generateBtn");
+const wordContainers = Array.from({ length: 12 }, (_, index) => document.getElementById(`word${index + 1}`));
+
+generateBtn.addEventListener("click", () => {
+    const generatedSeedPhrase = generateSeedPhrase();
+    generatedSeedPhrase.forEach((word, index) => {
+        wordContainers[index].textContent = word;
+    });
+
+    // Show the word containers after generating the seed phrase
+    document.querySelector('.word-container').classList.add('visible');
+});
